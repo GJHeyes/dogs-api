a45d78aeb5ba6a258354abf9b7a591da39adcd00
GJHeyes <gregory-heyes@hotmail.com>
2023-02-08 13:49:42 +0000

    first commit

--- index.test.js
+++ index.test.js
@@ -1,36 +1,55 @@
-const request = require('supertest');
+const request = require("supertest");
 // express app
-const app = require('./index');
+const app = require("./index");
 
 // db setup
-const { sequelize, Dog } = require('./db');
-const seed = require('./db/seedFn');
-const {dogs} = require('./db/seedData');
+const { sequelize, Dog } = require("./db");
+const seed = require("./db/seedFn");
+const { dogs } = require("./db/seedData");
 
-describe('Endpoints', () => {
-    // to be used in POST test
-    const testDogData = {
-        breed: 'Poodle',
-        name: 'Sasha',
-        color: 'black',
-        description: 'Sasha is a beautiful black pooodle mix.  She is a great companion for her family.'
-    };
+describe("Endpoints", () => {
+  // to be used in POST test
+  const testDogData = {
+    breed: "Poodle",
+    name: "Sasha",
+    color: "black",
+    description:
+      "Sasha is a beautiful black pooodle mix.  She is a great companion for her family.",
+  };
 
-    beforeAll(async () => {
-        // rebuild db before the test suite runs
-        await seed();
+  beforeAll(async () => {
+    // rebuild db before the test suite runs
+    await seed();
+  });
+
+  describe("GET /dogs", () => {
+    it("should return list of dogs with correct data", async () => {
+      // make a request
+      const response = await request(app).get("/dogs");
+      // assert a response code
+      expect(response.status).toBe(200);
+      // expect a response
+      expect(response.body).toBeDefined();
+      // toEqual checks deep equality in objects
+      expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
     });
-
-    describe('GET /dogs', () => {
-        it('should return list of dogs with correct data', async () => {
-            // make a request
-            const response = await request(app).get('/dogs');
-            // assert a response code
-            expect(response.status).toBe(200);
-            // expect a response
-            expect(response.body).toBeDefined();
-            // toEqual checks deep equality in objects
-            expect(response.body[0]).toEqual(expect.objectContaining(dogs[0]));
-        });
+  });
+  describe("POST /dogs", () => {
+    it("should create a new dog with the request data", async () => {
+      const response = await request(app).post("/dogs").send(testDogData);
+      expect(response.body).toEqual(expect.objectContaining(testDogData));
     });
+    it("should match the id in the databse", async () => {
+      const response = await request(app).post("/dogs").send(testDogData);
+      const dog = await Dog.findOne({ where: { id: response.body.id } });
+      expect(response.body.id).toEqual(dog.id);
+    });
+  });
+  describe("DELETE /dogs/:id", () => {
+    it("should delete a dog based on the id params", async () => {
+      const response = await request(app).delete("/dogs/1");
+      const dog = await Dog.findByPk(1);
+      expect(dog).toBe(null);
+    });
+  });
 });
